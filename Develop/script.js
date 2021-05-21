e58e3660db1b90757488a344a457e06bbd7d9ac3
Buney <dpkinser93@gmail.com>
2021-05-21 11:42:26 -0600

    time slots now are now different colors based on time of day

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,30 @@
 const timeOfDay = [
-    "9:00",
-    "10:00",
-    "11:00",
-    "12:00",
-    "1:00",
-    "2:00",
-    "3:00",
-    "4:00",
-    "5:00"
+    {time: "9:00", value: 9},
+    {time: "10:00", value: 10},
+    {time: "11:00", value: 11},
+    {time: "12:00", value: 12},
+    {time: "1:00", value: 13},
+    {time: "2:00", value: 14},
+    {time: "3:00", value: 15},
+    {time: "4:00", value: 16},
+    {time: "5:00", value: 17},
 ];
 
+// Get the .container div and save in a variable
 const $container = $('.container');
-
+// Use a loop to create time slots for each item in the timeOfDay array
 for (let i = 0; i < timeOfDay.length; i++) {
     const $timeBlock = $('<div>').addClass('row time-block');
     const $inputGroup = $('<div>').addClass('input-group');
-    const $hour = $('<p>').addClass('hour col-1').text(timeOfDay[i]);
+    const $hour = $('<p>').addClass('hour col-1').text(timeOfDay[i].time);
     const $textArea = $('<textarea>').addClass('form-control col-10');
+    if (timeOfDay[i].value === moment().hour()) {
+        $textArea.addClass('present');
+    } else if (timeOfDay[i].value > moment().hour()) {
+        $textArea.addClass('future');
+    } else {
+        $textArea.addClass('past');
+    }
     const $button = $('<button>').addClass('btn saveBtn col-1');
     $inputGroup.append($hour).append($textArea).append($button);
     $timeBlock.append($inputGroup);
